80b857f5e66918e5bfffc9ea5ed6b92f93dc2949
witchicken <witchicken2@gmail.com>
2022-11-07 14:24:35 +0900

    test-threejs1

--- react-ui/src/Threetest.js
+++ react-ui/src/Threetest.js
@@ -1,51 +1,5 @@
-import React, { Suspense } from "react";
-import { Canvas, useLoader } from "@react-three/fiber";
-import { OrbitControls } from "@react-three/drei";
-import { TextureLoader } from "three/src/loaders/TextureLoader";
-
-const name = (type) => `material/Rock050_1K_${type}.jpg`;
-
-function Scene() {
-  const [colorMap, displacementMap, normalMap, roughnessMap, aoMap] = useLoader(
-    TextureLoader,
-    [
-      name("Color"),
-      name("Displacement"),
-      name("NormalIDX"),
-      name("Roughness"),
-      name("AmbientOcclusion"),
-    ]
-  );
-  return (
-    <>
-      <ambientLight intensity={0.2} />
-      <directionalLight />
-      <mesh>
-        <sphereBufferGeometry args={[1, 100, 100]} />
-        <meshStandardMaterial
-          displacementScale={0.2}
-          map={colorMap}
-          displacementMap={displacementMap}
-          normalMap={normalMap}
-          roughnessMap={roughnessMap}
-          aoMap={aoMap}
-        />
-      </mesh>
-    </>
-  );
-}
-
+import React from "react";
 function Threetest() {
-  return (
-    <div className="Three">
-      <Canvas>
-        <Suspense fallback={null}>
-          <Scene />
-          <OrbitControls autoRotate />
-        </Suspense>
-      </Canvas>
-    </div>
-  );
+  return <>threetest123</>;
 }
-
 export default Threetest;
