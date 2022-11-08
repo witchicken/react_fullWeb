6aa3cded21e987b6a09856e7ab584cd741d86355
witchicken <witchicken2@gmail.com>
2022-11-08 16:01:29 +0900

    test-three

--- react-ui/src/Threetest.js
+++ react-ui/src/Threetest.js
@@ -1,5 +1,67 @@
-import React from "react";
-function Threetest() {
-  return <>threetest12322222</>;
+import React, { Suspense } from "react";
+import { Canvas, useLoader } from "@react-three/fiber";
+import { OrbitControls, useTexture } from "@react-three/drei";
+
+import { TextureLoader } from "three/src/loaders/TextureLoader";
+
+const name = (type) =>
+  `./material/PavingStones092_1K-JPG/PavingStones092_1K_${type}.jpg`;
+
+function Scene() {
+  const [colorMap, displacementMap, normalMap, roughnessMap, aoMap] = useLoader(
+    TextureLoader,
+    [
+      name("Color"),
+      name("Displacement"),
+      name("Normal"),
+      name("Roughness"),
+      name("AmbientOcclusion"),
+    ]
+  );
+  // const [
+  //   colorMap,
+  //   displacementMap,
+  //   normalMap,
+  //   roughnessMap,
+  //   aoMap
+  // ] = useTexture([
+  //   name("Color"),
+  //   name("Displacement"),
+  //   name("Normal"),
+  //   name("Roughness"),
+  //   name("AmbientOcclusion")
+  // ]);
+  return (
+    <>
+      <ambientLight intensity={0.2} />
+      <directionalLight />
+      <mesh>
+        {/* Width and height segments for displacementMap */}
+        <sphereBufferGeometry args={[1, 100, 100]} />
+        <meshStandardMaterial
+          displacementScale={0.2}
+          map={colorMap}
+          displacementMap={displacementMap}
+          normalMap={normalMap}
+          roughnessMap={roughnessMap}
+          aoMap={aoMap}
+        />
+      </mesh>
+    </>
+  );
 }
+
+function Threetest() {
+  return (
+    <div className="test">
+      <Canvas>
+        <Suspense fallback={null}>
+          <Scene />
+          <OrbitControls autoRotate />
+        </Suspense>
+      </Canvas>
+    </div>
+  );
+}
+
 export default Threetest;
